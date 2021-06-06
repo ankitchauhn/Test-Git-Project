3a236100a62272a70af1254f642a962b1f6cf8bd
ankit kumar <ankitkumar10199@gmail.com>
2021-06-06 16:58:23 +0530

    multiple file created

---
+++ Multi.java
@@ -0,0 +1,12 @@
+import jdk.internal.vm.PostVMInitHook;
+
+public class Multi 
+{
+    public static void main(String[] args) 
+    {
+        int a=2;
+        int b=3;
+        int c=a*b;
+        System.out.println(c);    
+    }    
+}
