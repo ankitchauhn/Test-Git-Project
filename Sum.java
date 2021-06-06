6c08ed42e5b4b588ac0b882049035cebde075351
ankit kumar <ankitkumar10199@gmail.com>
2021-06-06 16:15:39 +0530

    give message

--- Sum.java
+++ Sum.java
@@ -2,9 +2,9 @@ class Sum
 {
     public static void main(Stirng arsg[])
     {
-        int a=5;
+        int a=5 ;
         int b=5;
         int c=a+b;
-        System.out.print(c);
+        System.out.print("sum= "+ c);
     }
 }
